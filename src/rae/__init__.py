2b964b07dad313bfbe8a26f4c97421d2345d80ea
Ángel Carmona <angel@angelcarmona.com>
2026-03-25 05:37:41 +0100

    v0.9.2

--- src/rae/__init__.py
+++ src/rae/__init__.py
@@ -1,4 +1,8 @@
 import requests
+from bs4 import BeautifulSoup
+
+def clean_text(node):
+    return ' '.join(node.stripped_strings)
 
 def search_by_word(word):
     headers = {
@@ -8,7 +12,30 @@ def search_by_word(word):
     try:
         r = requests.get(f'https://dle.rae.es/{word}', headers=headers, timeout=5)
         r.raise_for_status()
-        return r.text
+        soup = BeautifulSoup(r.text, 'html.parser')
+        articles = []
+
+        for article in soup.select('article.o-main__article'):
+            title_node = article.select_one('h1.c-page-header__title')
+            intro_node = article.select_one('div.c-text-intro')
+
+            title = clean_text(title_node) if title_node else None
+            intro = clean_text(intro_node) if intro_node else None
+
+            definitions = []
+            for li in article.select('li.j'):
+                item = li.select_one('div.c-definitions__item')
+                if not item:
+                    continue
+                definitions.append(clean_text(item))
+
+            articles.append({
+                'title': title,
+                'intro': intro,
+                'definitions': definitions
+            })
+
+        return articles
     except requests.exceptions.Timeout:
         return None
     except requests.exceptions.HTTPError:
